c2c586c9b54b5712e0462f7aefae550753d4ecfb
NETDB <joshuapravin21@gmail.com>
2020-03-24 14:53:12 +0800

    Added api call to retrieve content from Airtable

--- src/App.js
+++ src/App.js
@@ -1,40 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col">
-          <div className="card-deck">
-            <div className="card">
-              <img className="card-img-top" src="https://via.placeholder.com/362x200" alt="Card image cap" />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-            <div className="card">
-              <img className="card-img-top" src="https://via.placeholder.com/362x200" alt="Card image cap" />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
-            <div className="card">
-              <img className="card-img-top" src="https://via.placeholder.com/362x200" alt="Card image cap" />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://api.airtable.com/v0/appxT2va454pW7x9i/favourites?api_key=keycR5bidgzjCEzFn')
+    .then((resp) => resp.json())
+    .then(data => {
+      this.setState({ movies: data.records });
+    }).catch(err => {
+      // Error message
+    });
+  }
+
+  render() {
+    return (
+      <div className="container mt-5 mb-5">
+        <div className="row">
+          <div className="col">
+            <div className="card-deck">
+              {this.state.movies.map(movie => <MovieCard {...movie.fields} />)}
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
+
+const MovieCard = ({ title, year, description, imageURL }) => (
+  <div className="card">
+    <img className="card-img-top" src={imageURL[0].url} alt="Movie Poster" />
+    <div className="card-body">
+    <h5 className="card-title">{title}</h5>
+    <p className="card-text">{description}</p>
+      <p className="card-text">
+        <small className="text-muted">{year}</small>
+      </p>
+    </div>
+  </div>
+);
